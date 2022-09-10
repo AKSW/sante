bc73635255bb323b6a3b6dd136404b1dae44aafc
hahojed979 <hahojed979@proton.me>
2022-09-10 11:29:17 +0200

    feat(dbpedia-lookup): add complete swagger annotations

---
+++ sante.api/src/main/java/org/aksw/sante/api/configuration/OpenApiDefinition.java
@@ -0,0 +1,14 @@
+package org.aksw.sante.api.configuration;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+
+@OpenAPIDefinition(
+		info = @Info(
+				title = "SANTé API",
+				version = "0.1",
+				description = "Documentation for SANTé API and all available endpoints"
+		)
+)
+public class OpenApiDefinition {
+}
--- sante.api/src/main/java/org/aksw/sante/api/dbpedia/DbpediaLookupController.java
+++ sante.api/src/main/java/org/aksw/sante/api/dbpedia/DbpediaLookupController.java
@@ -3,33 +3,78 @@ package org.aksw.sante.api.dbpedia;
 import java.util.*;
 
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.enums.ParameterIn;
+import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import org.springframework.http.MediaType;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
-
-// TODO Pascal/Upper-Camel-Case for query parameters
-// TODO Add request validation
-// TODO Handle Exceptions properly
-// TODO Complete Swagger documentation
+// TODO call the correct search method -> maybe add a that can be called without limit?
+// TODO Add request validation and exception handlers
+// TODO Pascal/Upper-Camel-Case for query parameters?
 
 @RestController
+@Validated
 public class DbpediaLookupController {
+
 	private final DbpediaLookupService dbpediaLookupService;
 
 	public DbpediaLookupController(DbpediaLookupService dbpediaLookupService) {
 		this.dbpediaLookupService = dbpediaLookupService;
 	}
+
 	@Operation(
-			summary = "Retrieve entities",
+			summary = "Retrieve entities in the format of DBpedia-Lookup",
+			description = "DBpedia Lookup provides a way of searching"
+					+ " through RDF data using natural language search queries."
+					+ " A user can therefore search for resources without having to use or learn SPARQL."
+					+ " This endpoint makes it possible to use SANTé in a similar fashion."
+					+ " It returns a search result that is equivalent of that of DBpedia Lookup.",
 			tags = {"dbpedia-lookup"},
+			parameters = {
+					@Parameter(
+							name = "maxHits",
+							description = "Limit the amount of results",
+							in = ParameterIn.QUERY
+					),
+					@Parameter(
+							name = "searchQuery",
+							description = "The natural language search query."
+									+ " Can be left empty, which will return all results.",
+							in = ParameterIn.QUERY
+					),
+					@Parameter(
+							name = "searchClasses",
+							description = "Comma separated list of classes which should be taken into consideration."
+									+ " Can be left empty, which will consider all classes",
+							in = ParameterIn.QUERY
+					)
+			},
 			responses = {
-					@ApiResponse(responseCode = "400", description = "Invalid input")
+					@ApiResponse(
+							responseCode = "200",
+							description = "The query was successful and returned a result."
+					),
+					@ApiResponse(
+							responseCode = "400",
+							description = "The request contains invalid input.",
+							content = @Content
+					),
+					@ApiResponse(
+							responseCode = "500",
+							description = "There was an issue while processing the query.",
+							content = @Content
+					)
 			}
 	)
-	@GetMapping(path = "/API/dbpedia-lookup")
+	@GetMapping(
+			path = "/api/dbpedia-lookup",
+			produces = MediaType.APPLICATION_JSON_VALUE)
 	@ResponseBody
 	protected DbpediaDocumentCollection lookupDbpedia(
-			@RequestParam(defaultValue = "10") Integer maxHits,
+			Integer maxHits,
 			@RequestParam(required = false) String searchQuery,
 			@RequestParam(required = false) String searchClasses
 	) throws Exception {
