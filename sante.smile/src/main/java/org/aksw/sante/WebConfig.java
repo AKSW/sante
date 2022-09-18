73efcfcd8c6615d89eb22be417d3d21feae7d662
Edgard Marx <digamarx@gmail.com>
2022-09-18 18:59:09 +0200

    adding missing files

---
+++ sante.smile/src/main/java/org/aksw/sante/WebConfig.java
@@ -0,0 +1,22 @@
+package org.aksw.sante;
+
+import java.util.Arrays;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+
+@Configuration
+public class WebConfig {
+    @Bean
+    MappingJackson2HttpMessageConverter customizedJacksonMessageConverter() {
+        MappingJackson2HttpMessageConverter converter = new MappingJackson2HttpMessageConverter();
+        converter.setSupportedMediaTypes(
+                Arrays.asList(
+                        MediaType.APPLICATION_JSON,
+                        new MediaType("application", "*+json"),
+                        MediaType.APPLICATION_OCTET_STREAM));
+        return converter;
+    }
+}
