1e87592e3ca2a232dea9d871074ca91dccc09bd2
hahojed979 <hahojed979@proton.me>
2022-12-05 15:31:56 +0100

    feat(configuration): refactor GeneralConfiguration

---
+++ sante.api/src/main/java/org/aksw/sante/api/configuration/GeneralConfiguration.java
@@ -0,0 +1,28 @@
+package org.aksw.sante.api.configuration;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Configuration class for Spring beans.
+ */
+@Configuration
+//@PropertySource("classpath:json-examples.properties")
+public class GeneralConfiguration {
+
+	/**
+	 * Configures the jackson object mapper that is used by default.
+	 * Sets deserialization to fail on unknown properties
+	 * and to exclude null-valued properties during serialization.
+	 *
+	 * @param defaultObjectMapper the object mapper that already exists in the IoC container
+	 */
+	@Autowired
+	public void configureObjectMapper(ObjectMapper defaultObjectMapper) {
+		defaultObjectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, true);
+		defaultObjectMapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
+	}
+}
